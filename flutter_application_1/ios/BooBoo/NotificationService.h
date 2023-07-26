6213d80f832f382ab180e004d924db4e022c4dbb
Bobby Beckmann <bobby@piels.com>
2023-07-26 10:44:08 -0500

    Error Demo

---
+++ flutter_application_1/ios/BooBoo/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  BooBoo
+//
+//  Created by Bobby Beckmann on 7/26/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
